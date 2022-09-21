8d8ae8187c3fb46f016ceee2f02ca36d018f104a
RajaArsalan814 <rarsalan0@gmail.com>
2022-09-21 22:53:13 +0800

    update

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -82,10 +82,6 @@ class EventController extends Controller
      */
     public function show($id)
     {
-<<<<<<< HEAD
-
-=======
->>>>>>> decdcb818cfc5f58a60bf47974e54cb6c5d176cd
         $event = Event::find($id);
         return view('event.show',compact('event'));
     }
